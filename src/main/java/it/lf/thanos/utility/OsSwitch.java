6146e0e4ebd1092c0c38555aa61d1c263261d913
Nimrak <Nimrak@shockwave.com>
2019-08-01 19:17:31 +0200

    Factory pattern for process utils selection

--- src/main/java/it/lf/thanos/utility/OsSwitch.java
+++ src/main/java/it/lf/thanos/utility/OsSwitch.java
@@ -1,22 +1,22 @@
 package it.lf.thanos.utility;
 
-import org.apache.commons.lang3.SystemUtils;
-
-import static it.lf.thanos.utility.ProcessUtilsCommon.*;
-import static it.lf.thanos.utility.ProcessUtilsLinux.*;
-import static it.lf.thanos.utility.ProcessUtilsWindows.*;
+import static it.lf.thanos.utility.Randomizer.createRandomList;
 
 import java.util.List;
 
+import org.apache.commons.lang3.SystemUtils;
+
+import it.lf.thanos.utility.pu.IProcessUtils;
+import it.lf.thanos.utility.pu.ProcessUtilsFactory;
+import it.lf.thanos.utility.pu.ProcessUtilsFactory.OS;
+
 public class OsSwitch {
-    public static void searchOs(boolean isHard) throws InterruptedException {
-        List<String> randomList;
-        if (SystemUtils.IS_OS_WINDOWS) {
-            randomList = createRandomList(listRunningProcessesWin());
-            execKillWin(randomList, isHard);
-        } else if (SystemUtils.IS_OS_LINUX || SystemUtils.IS_OS_MAC) {
-            randomList = createRandomList(listRunningProcessLinux());
-            execKillLinux(randomList, isHard);
-        }
-    }
+	public static void searchOs(boolean isHard) throws InterruptedException {
+		OS os = SystemUtils.IS_OS_WINDOWS ? OS.WIN : (SystemUtils.IS_OS_LINUX || SystemUtils.IS_OS_MAC) ? OS.LINUX : null;
+
+		IProcessUtils utils = ProcessUtilsFactory.getProcessUtils(os);
+
+		List<String> randomList = createRandomList(utils.listRunningProcesses());
+		utils.execKill(randomList, isHard);
+	}
 }
